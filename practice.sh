e8cd62d264990ce5032193d26578338c6a7498cd
Anand Zupa <anzu7346@colorado.edu>
2019-09-20 11:33:33 -0600

    Creating practice.sh file and storing regex practice.sh

---
+++ practice.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Author : Anand Zupa 
+# Date : 9/20/2019
+# Script follows here:
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+let prod=numOne*numTwo
+echo "The product is: $prod"
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+~                                                                              
+-- INSERT --                                                 11,29         All
+

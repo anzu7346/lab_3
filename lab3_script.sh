019a7daf780c14e1217f84e2f366adc9da1940d1
Anand Zupa <anzu7346@colorado.edu>
2019-09-22 21:30:11 -0600

    The final grep file as well as the txt for emails from geocities.

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,5 +4,12 @@
 echo "Enter a file name and a regular expression. "
 read fil
 read regz
-grep fil regz
+#grep -c $regz $fil
+# Email count mod below
+#grep -c "@" $fil
 
+# Phone number mod below
+#grep -o [0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\} $fil
+
+#Email output
+grep "@geocities.com" $fil >> "email_results.txt"
